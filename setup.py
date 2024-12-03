7599667f1a74a3e54271d617ca0071554f8d25b9
EricBoittier <eric.boittier@icloud.com>
2024-12-03 16:32:10 +0100

    install

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="physnet_jax_pycharmm",
+    version="0.1.0",
+    author="Eric Boittier",
+    author_email="my.email@example.com",
+    description="A brief description of your package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/physnet_jax_pycharmm",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=[
+        # "jax",
+        # "pycharmm",
+        # Add other dependencies here
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires=">=3.7",
+)
