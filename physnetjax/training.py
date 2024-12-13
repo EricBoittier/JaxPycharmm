c5ac3eae4b56a839a42d9f413742736ec80b3a58
Eric Boittier <ericdavid.boittier@unibas.ch>
2024-12-13 23:14:05 +0100

    updated batches

--- physnetjax/training.py
+++ physnetjax/training.py
@@ -27,13 +27,13 @@ from physnetjax.tensorboard_logging import write_tb_log
 from physnetjax.trainstep import train_step
 
 # from physnetjax.utils import get_last, get_params_model, pretty_print
-from physnetjax.pretty_printer import (
-    init_table,
-    epoch_printer,
-    training_printer,
-    Printer,
-    print_dict_as_table,
-)
+# from physnetjax.pretty_printer import (
+#     init_table,
+#     epoch_printer,
+#     training_printer,
+#     Printer,
+#     print_dict_as_table,
+# )
 from physnetjax.restart import restart_training, orbax_checkpointer
 
 from physnetjax.utils import create_checkpoint_dir
