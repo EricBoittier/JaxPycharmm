6e24c15297b48e7143726307fdc57a09627f88bd
Eric Boittier <ericdavid.boittier@unibas.ch>
2024-12-13 11:43:34 +0100

    .

--- physnetjax/model.py
+++ physnetjax/model.py
@@ -176,8 +176,9 @@ class EF(nn.Module):
         """Perform one iteration of message passing."""
         if iteration == self.num_iterations - 1:
             x = e3x.nn.MessagePass(max_degree=0, include_pseudotensors=False,
-                                   dense_kernel_init=jax.nn.initializers.he_normal,
-                                   dense_bias_init=jax.nn.initializers.he_normal)(
+                                   # dense_kernel_init=jax.nn.initializers.he_normal,
+                                   # dense_bias_init=jax.nn.initializers.he_normal
+                                   )(
                 x, basis, dst_idx=dst_idx, src_idx=src_idx
             )
             return e3x.nn.change_max_degree_or_type(
@@ -185,8 +186,9 @@ class EF(nn.Module):
             )
 
         return e3x.nn.MessagePass(include_pseudotensors=False,
-                                  dense_kernel_init=jax.nn.initializers.he_normal,
-                                   dense_bias_init=jax.nn.initializers.he_normal)(
+                                  # dense_kernel_init=jax.nn.initializers.he_normal,
+                                  #  dense_bias_init=jax.nn.initializers.he_normal
+                                  )(
             x, basis, dst_idx=dst_idx, src_idx=src_idx
         )
 
