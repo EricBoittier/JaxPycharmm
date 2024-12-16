d6343424ae61d4a69ce2a34a03f6789af97866cd
Eric Boittier <ericdavid.boittier@unibas.ch>
2024-12-16 22:04:42 +0100

    .

--- physnetjax/sim/pycharmm_sim.py
+++ physnetjax/sim/pycharmm_sim.py
@@ -60,11 +60,12 @@ def setup_coordinates(pdb_file, psf_file, atoms):
     settings.set_warn_level(-1)
     read.pdb(pdb_file, resid=True)
     read.psf_card(psf_file)
+    atoms = ase.io.read(pdb_file)
     # add_waters()
-    R = coor.get_positions().values
+    # R = coor.get_positions().values
     import pycharmm.psf as psf
     Z = [str(_)[:1] for _ in psf.get_atype()]
-    # atoms = ase.Atoms(Z, R)
+    atoms = ase.Atoms(Z, atoms.get_positions())
     coor.set_positions(pd.DataFrame(atoms.get_positions(), columns=["x", "y", "z"]))
     return atoms
 
