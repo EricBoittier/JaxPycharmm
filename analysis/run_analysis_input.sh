658ef087672a68d3c9473f7be7ee6446bf0c840c
Eric Boittier <ericdavid.boittier@unibas.ch>
2024-12-16 18:56:02 +0100

    .

---
+++ analysis/run_analysis_input.sh
@@ -0,0 +1,4 @@
+   ./run_analysis.sh \
+       /pchem-data/meuwly/boittier/home/pycharmm_test/ckpts/cf3all-e94f959c-5f22-4b8a-800e-dd6197c07b20 \
+       /pchem-data/meuwly/boittier/home/jaxeq/notebooks/ala-esp-dip-0.npz \
+       8000 1786 47
