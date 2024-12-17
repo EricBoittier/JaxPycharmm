032f87924c84c9ba5bc7e6c06cb7b3cee106524a
EricBoittier <eric.boittier@icloud.com>
2024-12-17 15:14:52 +0100

    uv

--- physnetjax/analysis/povray_tool.py
+++ physnetjax/analysis/povray_tool.py
@@ -56,8 +56,8 @@ def render_povray(atoms, pov_name,
     # create hydrogen bonds
     _pos = atoms.get_positions()
     _z = atoms.get_atomic_numbers()
-    print(np.where(_z == 8))
-    idx_onh = (np.where(_z == 8)[1] | np.where(_z == 1)[1]) | np.where(_z == 7)[1]
+    idx_onh = ((_z == 8) | (_z == 1) | (_z == 7))
+    print(idx_onh)
     atoms_onh = Atoms(_pos[idx_onh], _z[idx_onh])
     bondpairs_onh = get_bondpairs(atoms_onh, radius=2.0)
     for _ in bondpairs_onh:
