0071d09bf9f737de05f92c87662f41a576f9e0f3
EricBoittier <eric.boittier@icloud.com>
2024-12-17 15:32:39 +0100

    uv

--- physnetjax/analysis/povray_tool.py
+++ physnetjax/analysis/povray_tool.py
@@ -43,7 +43,7 @@ def render_povray(atoms, pov_name,
 
     colors = np.array([color_dict[atom.symbol] for atom in atoms]) / 255
 
-    bondpairs = get_bondpairs(atoms, radius=1.0)
+    bondpairs = get_bondpairs(atoms, radius=0.9)
     good_bonds = []
     good_bond_keys = []
     for _ in bondpairs:
