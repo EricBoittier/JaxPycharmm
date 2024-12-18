d5d6784c7b039b6aaf59af0c377c29d5035acb1f
Eric Boittier <ericdavid.boittier@unibas.ch>
2024-12-18 01:29:58 +0100

    ...

--- physnetjax/utils/pretty_printer.py
+++ physnetjax/utils/pretty_printer.py
@@ -39,7 +39,7 @@ def get_acp_plot(data, keys, title="", log=False):
         'format': '{:1.2e}',
         # "colors": [acp_colors[i] for i in range(len(keys))],
     }
-    skip = 0 if len(data) < 100 else len(data) // 50
+    skip = 0 if len(data) < 100 else len(data) // 80
     data_to_plot = [list(data[key].drop_nulls().to_numpy())[::skip] for key in keys]
     print(len(data_to_plot[0]))
     p = Panel(
