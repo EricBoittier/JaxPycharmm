541977d981a231ee783ae2f358615d607fec1688
Eric Boittier <ericdavid.boittier@unibas.ch>
2024-12-18 01:07:28 +0100

    ...

--- physnetjax/utils/pretty_printer.py
+++ physnetjax/utils/pretty_printer.py
@@ -3,6 +3,7 @@ from rich.table import Table
 import time
 import time
 from rich.panel import Panel
+from rich.console import Console
 from rich.live import Live
 from rich.table import Table
 import asciichartpy as acp
