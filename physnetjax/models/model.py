905857a54f8609b1ed4cf0962509c105431b1c89
Eric Boittier <ericdavid.boittier@unibas.ch>
2024-12-19 01:48:07 +0100

    ...

--- physnetjax/models/model.py
+++ physnetjax/models/model.py
@@ -582,8 +582,8 @@ class EF(nn.Module):
         energy_and_forces = jax.value_and_grad(self.energy, argnums=1, has_aux=True)
 
         # Debug input shapes
-        # if isinstance(self.debug, list) and "idx" in self.debug:
-        if True:
+        if isinstance(self.debug, list) and "idx" in self.debug:
+        # if True:
             jax.debug.print("atomic_numbers {x}", x=atomic_numbers.shape)
             jax.debug.print("positions {x}", x=positions.shape)
             jax.debug.print("dst_idx {x}", x=dst_idx.shape)
@@ -639,8 +639,8 @@ class EF(nn.Module):
         }
         # print("output", output)
         # Debug output values
-        # if isinstance(self.debug, list):
-        if True:
+        if isinstance(self.debug, list):
+        # if True:
             if "forces" in self.debug:
                 debug_forces(output, forces)
             if "energy" in self.debug:
