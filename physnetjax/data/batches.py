97097c5984e38912ca8a5de9f6b495b1e421b726
EricBoittier <eric.boittier@icloud.com>
2024-12-19 21:50:48 +0100

    ...

--- physnetjax/data/batches.py
+++ physnetjax/data/batches.py
@@ -293,7 +293,7 @@ def create_batch(perm, dst_src_lookup, data, data_keys,
     an_counter = 0
     for i, n_atoms in enumerate(n):
         n_atoms = int(n_atoms)
-        if n_atoms == 0 or n_atoms > batch_size:
+        if n_atoms == 0 or n_atoms > batch_shape:
             raise ValueError(f"Invalid number of atoms: {n_atoms}")
         tmp_dst, tmp_src = dst_src_lookup[int(n_atoms)]
         len_current_nbl = int(n_atoms) * (int(n_atoms) - 1)
