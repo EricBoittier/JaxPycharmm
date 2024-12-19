6ba04dc40150a3da8de3f14218fe19013393bb65
EricBoittier <eric.boittier@icloud.com>
2024-12-19 15:24:04 +0100

    ...

--- physnetjax/training/optimizer.py
+++ physnetjax/training/optimizer.py
@@ -37,7 +37,7 @@ def get_optimizer(
     **kwargs,
 ):
     if isinstance(clip_global, bool):
-        clip_global = 1.0 if clip_global else None
+        clip_global = 10.0 if clip_global else None
 
     if schedule_fn is None:
         schedule_fn = optax.schedules.constant_schedule(learning_rate)
