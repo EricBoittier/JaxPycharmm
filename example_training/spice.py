d83199ffe5f8695f03860067a8db03acd65a4054
Eric Boittier <ericdavid.boittier@unibas.ch>
2024-12-20 03:45:41 +0100

    batching

--- example_training/spice.py
+++ example_training/spice.py
@@ -16,7 +16,7 @@ NTRAIN = 1000
 NVALID = 1000
 DEFAULT_DATA_KEYS = ("Z", "R", "D", "E", "F", "N")
 RANDOM_SEED = 42
-BATCH_SIZE = 30
+BATCH_SIZE = 20
 
 # # Environment configuration
 # os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"] = ".99"
