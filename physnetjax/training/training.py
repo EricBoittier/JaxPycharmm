8d970114c30f324ef874dfcf863254fa54cc46e6
Eric Boittier <ericdavid.boittier@unibas.ch>
2024-12-20 01:57:54 +0100

    batching

--- physnetjax/training/training.py
+++ physnetjax/training/training.py
@@ -196,16 +196,15 @@ def train_model(
         kwargs.update(batch_args_dict)
     valid_batches = _prepare_batches(kwargs)
 
-    print_shapes(valid_batches[0], name="Validation Batches")
-
-    console.print(p_(lj.lovely(valid_data["E"]), title="Energy"))
-    console.print(p_(lj.lovely(valid_data["F"]), title="Forces"))
+    print_shapes(valid_batches[0], name="Validation Batch[0]")
+    jax.debug.print("Z: {x}", x=valid_data["Z"])
+    jax.debug.print("R: {x}", x=valid_data["R"])
+    jax.debug.print("E: {x}", x=valid_data["E"])
+    jax.debug.print("F: {x}", x=valid_data["F"])
     if model.charges:
-        console.print(p_(lj.lovely(valid_data["D"]), title="Dipoles"))
-    console.print(p_(lj.lovely(valid_data["Z"]), title="Atomic Numbers"))
-    console.print(p_(lj.lovely(valid_data["R"]), title="Positions"))
-    console.print(p_(lj.lovely(valid_data["src_idx"]), title="Source Indices"))
-    console.print(p_(lj.lovely(valid_data["dst_idx"]), title="Destination Indices"))
+        jax.debug.print("D: {x}", x=valid_data["D"])
+    jax.debug.print("src_idx: {x}", x=valid_data["src_idx"])
+    jax.debug.print("dst_idx: {x}", x=valid_data["dst_idx"])
 
     dst_idx, src_idx = e3x.ops.sparse_pairwise_indices(num_atoms)
     params = model.init(
